7714a90a5736da8c97a65a278cdea2fb4919dbb6
tushar26g <tushar26g@gmail.com>
2024-05-04 14:22:41 +0530

    Edit the NavBar

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -14,39 +14,41 @@ const NavBar = ({theme, setTheme}) => {
   const[menuOpen, setMenuOpen]=useState(false);
 
   return (
-        <div className={`navbar ${theme}`}>
-            <div className="logo">
-              <h1>
-                <span>P</span>aisaTo
-                <span>P</span>aisa
-              </h1>
-            </div>
+    <nav className={`navbar ${theme}`}>
+      <div className='NavBar'>
+        <div className='logo' to="/">
+          <h1>
+            <span>P</span>aisaTo
+            <span>P</span>aisa
+          </h1>
+        </div>
+      </div>
 
-            <div className='Menu' onClick={()=>{
+      <div className='Menu' onClick={()=>{
               setMenuOpen(!menuOpen);
             }}>
               <span></span>
               <span></span>
               <span></span>
-            </div>
+      </div>
 
-
-              <ul className={menuOpen ? "open" : ""}>
-                <li>Explore</li>
-                <li>Foundation</li>
-                <li>Inter</li> 
-                <li>Final</li>
+      <div className={`right-menu ${menuOpen ? "open" : ""}`}>
+          <ul>
+                <li>Home</li>
+                <li>CA Hub</li>
+                <li>Store</li>
+                <li>About</li>
                 <div className='search-box'>
                 <input type="text" placeholder='Search'/>
                 <img src={theme === 'light' ? search_dark : search_light} alt="" />
               </div>
               <img onClick={()=>{toggle_mode()}} src={theme === 'light' ? logo_light : logo_dark} alr="" className='toggle-icon'/>
-
-              <div>
-                <button className='Login'>login</button>
-              </div>
-              </ul>
-        </div>
+                <div>
+                    <button className='Login'>login</button>
+                </div>
+          </ul>
+      </div>
+    </nav>
   )
 }
 
