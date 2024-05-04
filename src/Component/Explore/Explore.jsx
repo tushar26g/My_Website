345f6b5f6e855c34d132a58eca84cee57b2d3db2
tushar26g <tushar26g@gmail.com>
2024-05-04 21:41:06 +0530

    Added Home Page(Explore)

--- src/Component/Explore/Explore.jsx
+++ src/Component/Explore/Explore.jsx
@@ -1,17 +1,80 @@
 import React from 'react'
 import './Explore.css'
+import bcom from '../../Assets/Home/bcom.png'
+import caHub from '../../Assets/Home/group.jpg'
+import articalship from '../../Assets/Home/articalship.jpg'
+import CA from '../../Assets/Home/CA2.jpg'
+import intervivew from '../../Assets/Home/intervivew.jpg'
+import tax from '../../Assets/Home/Tax.png'
+import inter from '../../Assets/Home/Inter.jpg'
+import found from '../../Assets/Home/foundation.png'
 
 const Basic = () => {
   return (
     <div className='Basic'>
-        <button className='HomeButton'>CA Gang</button>
-        <button className='HomeButton'>Intervivew</button>
-        <button className='HomeButton'>Tax Law</button>
-        <button className='HomeButton'>Articalship</button>
-        <button className='HomeButton'>Study Material</button>
-        <button className='HomeButton'>Question Bank</button>
-        <button className='HomeButton'>PYQ</button>
-        <button className='HomeButton'>B.Com</button>
+        <div className='HomeButton'>
+          <div><img src={caHub} alt="Example" /></div>
+          <div className='content'>
+            <h3>CA Hub</h3>
+            <p>All CA</p>
+          </div>
+        </div>
+        
+        <div className='HomeButton'>
+        <div><img src={found} alt="Example" /></div>
+          <div className='content'>
+            <h3>CA foundation</h3>
+            <p>CA Final exam study material, question bank, PYQ</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={inter} alt="Example" /></div>
+          <div className='content'>
+            <h3>CA Inter</h3>
+            <p>CA Inter exam study material, question bank, PYQ</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={CA} alt="Example" /></div>
+          <div className='content'>
+            <h3>CA Final</h3>
+            <p>CA Final exam study material, question bank, PYQ</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={tax} alt="Example" /></div>
+          <div className='content'>
+            <h3>Tax Laws</h3>
+            <p>All tax law</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={articalship} alt="Example" /></div>
+          <div className='content'>
+            <h3>Articleship</h3>
+            <p>All the tools info use in Articalship</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={intervivew} alt="Example" /></div>
+          <div className='content'>
+            <h3>Job preparation</h3>
+            <p>Imp interview questions on all sub required job</p>
+          </div>
+        </div>
+
+        <div className='HomeButton'>
+        <div><img src={bcom} alt="Example" /></div>
+          <div className='content'>
+            <h3>B.Com</h3>
+            <p>B.Com Projects, study material, question bank</p>
+          </div>
+        </div>
     </div>
   )
 }
