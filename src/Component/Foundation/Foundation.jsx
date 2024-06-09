3d5775384f2ba2b32dc25ff183267f21db262501
tushar26g <tushar26g@gmail.com>
2024-06-09 12:43:16 +0530

    Edited foundation

--- src/Component/Foundation/Foundation.jsx
+++ src/Component/Foundation/Foundation.jsx
@@ -5,6 +5,16 @@ import appt from '../../Assets/Foundation/Apptie.jpg'
 import buisness from '../../Assets/Foundation/BuisnessLaw.jpg'
 import buiEco from '../../Assets/Foundation/BusinessEconomics.jpg'
 
+import accountingQB from '../../Assets/Foundation/AccountingQB.jpg'
+import apptQB from '../../Assets/Foundation/ApptieQB.jpg'
+import buisnessQB from '../../Assets/Foundation/BLQB.jpg'
+import buiEcoQB from '../../Assets/Foundation/BEQB.jpg'
+
+import accountingPYQ from '../../Assets/Foundation/AccountingPYQ.jpg'
+import apptPYQ from '../../Assets/Foundation/ApptiePYQ.jpg'
+import buisnessPYQ from '../../Assets/Foundation/BLPYQ.jpg'
+import buiEcoPYQ from '../../Assets/Foundation/BEPYQ.jpg'
+
 const Foundation = () => {
   return (
     <div className="grid-container">
@@ -15,27 +25,27 @@ const Foundation = () => {
         <div className='SubSection'>
             <div className='Sub'>
                 <div><img src={accounting} alt="Example" /></div>
-                <div className='content'>
+                <div className='subName'>
                   <h3>Accounting</h3>
                 </div>
             </div>
 
             <div className='Sub'>
             <div><img src={buisness} alt="Example" /></div>
-                <div className='content'>
+                <div className='subName'>
                   <h3>Business Law</h3>
                 </div>
             </div>
             
             <div className='Sub'>
             <div><img src={appt} alt="Example" /></div>
-                <div className='content'>
+                <div className='subName'>
                   <h3>Quantitative Aptitude</h3>
                 </div>
             </div>
             <div className='Sub'>
             <div><img src={buiEco} alt="Example" /></div>
-                <div className='content'>
+                <div className='subName'>
                   <h3>Business Economics</h3>
                 </div>
             </div>
@@ -46,22 +56,26 @@ const Foundation = () => {
         <h2>Question Bank</h2>
         <div className='SubSection'>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={accountingQB} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Accounting</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={buisnessQB} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Business Law</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={apptQB} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Quantitative Aptitude</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={buiEcoQB} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Business Economics</h3>
                 </div>
             </div>
@@ -72,22 +86,26 @@ const Foundation = () => {
         <h2>PYQ</h2>
         <div className='SubSection'>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={accountingPYQ} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Accounting</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={buisnessPYQ} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Business Law</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={apptPYQ} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Quantitative Aptitude</h3>
                 </div>
             </div>
             <div className='Sub'>
-                <div className='content'>
+            <div><img src={buiEcoPYQ} alt="Example" /></div>
+                <div className='subName'>
                   <h3>Business Economics</h3>
                 </div>
             </div>
